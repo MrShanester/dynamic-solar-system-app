73c01842687d435b4d41edcb190638ea787f6dbb
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-19 10:31:14 -0800

    Adds CRUD routes and actions for planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -3,4 +3,38 @@ class PlanetsController < ApplicationController
     planets = Planet.all 
     render json: planets.as_json
   end
+
+  def show
+    planet = Planet.find_by(id: params[:id])
+    render json: planet.as_json
+  end
+
+  def create
+    planet = Planet.new(system_id: params[:system_id], name: params[:name], diameter: params[:diameter], mass: params[:mass], orbital_distance: params[:orbital_distance], description: params[:description], image: params[:image], is_star: params[:is_star])
+    if planet.save
+      render json: planet.as_json
+    else 
+      render json: {error: planet.error.full_messages}
+    end
+  end
+
+  def update
+    planet = Planet.find_by(id: params[:id])
+    planet.system_id = planet.system_id
+    planet.name = params[:name] || planet.name
+    planet.diameter = params[:diameter] || planet.diameter
+    planet.mass = params[:mass] || planet.mass
+    planet.orbital_distance = params[:orbital_distance] || planet.orbital_distance
+    planet.description = params[:description] || planet.description
+    planet.image = params[:image] || planet.image
+    planet.is_star = params[:is_star] || planet.is_star
+  end
+
+  def destroy
+    planet = Planet.find_by(id: params[:id])
+    if planet.delete
+      render json: {message: "Planet Deleted"}
+    else  
+      render json: {error: planet.error.full_messages}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/planets" => "planets/index"
+  get "/planets" => "planets#index"
+  get "/planets/:id" => "planets#show"
+  post "/planets" => "planets#create"
+  patch "/planets" => "planets#update"
+  delete "/planets" => "planets#destroy"
+  
 end
